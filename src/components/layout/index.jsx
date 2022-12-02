da4bb34e1bf6a575377af9aeee0b611ba942bae8
Santiago Souto <souto.santiago@academico.ifpb.edu.br>
2022-12-02 00:49:58 -0300

    Parte do calculo e apresentação dos dados pronta

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from './style'
 
 export const Layout = () => {
+
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0.0);
+    const [BodyMassIndex, setBodyMassIndex] = useState(0);
+
+    useEffect(() => {
+        setBodyMassIndex(weight / ((height / 100) * (height / 100)))
+    }, [weight, height]);
+
     return (
         <Container>
+            <span>{weight}Kg</span>
+            <input type="range" min="0" max="394" value={weight} onChange={(e) => setWeight(e.target.value)} />
+            <span>{(height / 100).toFixed(2)}M</span>
+            <input type="range" min="0" max="238" value={height} onChange={(e) => setHeight(e.target.value)} />
+            <span>{BodyMassIndex.toFixed(2)}</span>
         </Container>
     )
 }
--- src/components/layout/style.jsx
+++ src/components/layout/style.jsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-    
+    margin: 0 auto;
+    width: 98%;
+    max-width: 800px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
