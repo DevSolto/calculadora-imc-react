963b28ca39906f3821e855e51ea6caa100236540
Santiago Souto <souto.santiago@academico.ifpb.edu.br>
2022-12-02 00:10:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Layout } from "./components/layout";
+import { GlobalStyle } from "./globalStyle";
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/layout/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Container } from './style'
+
+export const Layout = () => {
+    return (
+        <Container>
+        </Container>
+    )
+}
